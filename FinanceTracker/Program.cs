298d6c6adcb72c61d3386ac0a973659962dad256
Sheetal <sheetalrana190@gmail.com>
2025-03-11 17:18:32 -0400

    Add project file FinanceTracker and .gitignore

---
+++ FinanceTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
