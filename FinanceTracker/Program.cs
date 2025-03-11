6ecc0a81b995fb76d53db35825d249b5eaffae78
urvisurti897 <urvisurti897@gmail.com>
2025-03-11 18:04:58 -0400

    Hello Everyone

--- FinanceTracker/Program.cs
+++ FinanceTracker/Program.cs
@@ -10,6 +10,8 @@
             Console.WriteLine("Hello, World! !!! !!! ");
 
             Console.WriteLine("Hello, World ####  ");
+
+            Console.WriteLine("Hello Everyone ");
         }
     }
 }
